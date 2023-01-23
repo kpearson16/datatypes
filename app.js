30ffe34b85b858e69f3567f9a430ab8398927a37
kpearson16 <122332523+kpearson16@users.noreply.github.com>
2023-01-22 23:39:23 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+//*Create 10 variables of EACH datatype [String,Integer,Array,Object]*//
+//*String*//
+const name = 'Keamber';
+const school = "Georgia State University";
+const height = "5'5";
+const y = 'INFJ';
+const hometown = "Haddock, Georgia";
+let favColor = 'blue';
+let carType = 'Hyundai Veloster';
+let city = 'Atlanta';
+let weight = '158lbs';
+let hairStyle = 'locs';
+
+//*integers*//
+let x = 199;
+let x = 1.2;
+let x = 999e7;
+let a = 57;
+let b = 3;
+let c = a + b;
+let pie = 3.;
+let myBirthMonth = 6;
+let myBirthDay = 9;
+let mySisters = 4;
+
+//*array*//
+
+let Siblings = ["Tevia", "Twon", "Drea", "Chaz", "Niya"];
+const parents = ["Akito", "Katina"];
+const Grandparents = ["Christine", "Harry Sr.", "Bettie", "Arthur"];
+let sizes = ["8.5", "9", "10"];
+const brands = ['Nikon', 'Canon', 'Fujifilm', 'Sony', 'Pentax', 'Mamiya'];
+let foods = ['sandwich', 'pizza', 'pasta', 'nachos'];
+let parks = ['Piedmont', 'Adair', 'West End'];
+const states = ['Florida', 'Wyoming', 'Illinois']
+let shows = ['NCIS', 'Criminal Minds', 'Law N Order']
+const characters = ['Spongebob','Squidward', 'Patrick', 'Plankton']
+
+//*objects*//
+const pet = {firstName:"Dulce", lastName:"Pearson", age:4, type:"tabby"};
+const college = {
+    school:"GSU",
+    major:"African American Studies",
+    minor:"Gender and Sexuality Studies",
+    GPA: "3.7",
+    gradYear:"2018"
+};
+let funFacts = {favTea:'jasmine', age:"26"};
+let vacations = {first: "Cancun", year:"2020", guests:"Chelsea and Robin"};
+let recipes = {holiday:"Thanksgiving", location:"Grama's", guests:"42"}
+let plants = {first:"aloe", number:"14", favorite:"peace lily"};
+const professors1 = {name:"Dr.Simanga", courses:"Creative Writing, AA History", tenured:"no"};
+const pastResidences = {city:"Brooklyn", hometown:"Haddock", dates:"2020-2022"};
+let restaurants= {fav:"Malai Marke", leastFav:"SEA", visits:"5"};
+const cars = {first:"Cobalt", year:"2007", current:"veloster", seats:"4"};
